1cd63d24650c9486bf922e3698e499daba77115e
Hunter <hdog0915@gmail.com>
2020-06-01 11:53:32 -0400

    Create menu to select genre

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,7 @@
-alert("Test");
+var checkboxes = document.getElementsByClassName("genre-checkbox");
+console.log(checkboxes);
+for (var i = 0; i < checkboxes.length; ++i)
+{
+	checkboxes[i].indeterminate = true;
+	console.log(checkboxes[i]);
+}
